247aab7909908249f774052cdbd2859fcd3eef54
acleitner1 <78944987+acleitner1@users.noreply.github.com>
2024-05-25 08:24:40 -0400

    Todo list

--- compressor.py
+++ compressor.py
@@ -2,6 +2,16 @@
 # Anna Leitner, May/June 2024
 import sys, os
 
+#Todo: 
+# 1. Build binary tree 
+# 2. Generate prefix codes based on binary tree 
+# 3. Header section for output file
+# 4. Encode text using code table and write it to output file 
+#     -> Be sure to translate prefixes into bit string and pack into bytes to achieve compression 
+# 5. Read header and rebuild prefix table 
+# 6. Decode the text and write it to specified output file.
+#     -> If this works, should be able to take a file, encode it, check that the new file is smaller 
+#        than the original, then decode that file into a file identical to the original. 
 def main(input): 
    # Main function: It has been a long time since I did anything in python....
    if len(input.argv) != 2: 
@@ -17,9 +27,11 @@ def main(input):
          else: 
             character_map[character] = 1 
    f.close()
+   build_binary_tree(character_map)
 
-   print(character_map["t"])
-   print(character_map['X'])
 
+def build_binary_tree(character_map): 
+   #TODO 
+   return
 # Call main on command line input 
 main(sys)
